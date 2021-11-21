94b026d912c86e29b2df798de3ff5af05c7fd58d
Katie <katie.herda2@gmail.com>
2021-11-21 22:19:39 +0000

    added holders for links in navigation bar.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import PostList from './components/Posts/PostList';
+import NavBar from './components/Nav/NavBar';
 
 function App() {
   return (
     <div className="App">
+      < NavBar />
       < PostList />
     </div>
   );
---
+++ src/components/Nav/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function NavBar(props) {
+  return (
+    <section className="navbar">
+    
+        <a href="/home">Home</a>
+        <a href="/Profile">Profile</a>
+        <a href="/Settings">Settings</a>
+        <a href="/Login">Login</a>
+        <a href="/Signup">Signup</a>
+  
+    </section>
+  )
+}
