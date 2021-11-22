6eda550ab321bc14be627071a4b15be47f6b3adf
Katie <katie.herda2@gmail.com>
2021-11-22 01:14:49 +0000

    imported scss files to .js files.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import PostList from './components/Posts/PostList';
 import NavBar from './components/Nav/NavBar';
 
--- src/components/Nav/NavBar.js
+++ src/components/Nav/NavBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './NavBar.scss'
 
 export default function NavBar(props) {
   return (
