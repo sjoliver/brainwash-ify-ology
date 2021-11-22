4c859bb8a4a1d1490c0b4787352f9f337501fb5c
rengebre <russell.engebretson@gmail.com>
2021-11-22 11:47:59 -0800

    Add interest state and useEffect on initial load of page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import './App.scss';
 import NavBar from './components/Nav/NavBar';
 import PostIndex from './components/PostIndex/PostIndex';
@@ -6,11 +8,20 @@ import PostForm from './components/PostForm/PostForm';
 
 
 function App() {
+  const [interests, setInterests] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3000/interests')
+      .then(res => { setInterests(() => res.data) })
+      .catch(e => console.error(e))
+  }, []);
+
   return (
     <div className="App">
       < NavBar />
-      < PostForm />
-      < PostIndex />
+      < PostForm interests={interests}/>
+      < PostIndex interests={interests}/>
     </div>
   );
 }
