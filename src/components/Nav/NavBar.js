8117919f1ffa9ab533dd301152587f66828b8880
rengebre <russell.engebretson@gmail.com>
2021-11-23 09:47:35 -0800

    implement oAuth through Auth0. Add login/logout buttons

--- src/components/Nav/NavBar.js
+++ src/components/Nav/NavBar.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import './NavBar.scss'
 
+import LoginButton from './LoginButton';
+import LogoutButton from './LogoutButton';
+import Profile from './Profile';
+
 export default function NavBar(props) {
   return (
     <section className="navbar">
     
         <a href="/home">Home</a>
-        <a href="/Profile">Profile</a>
+        <Profile />
         <a href="/Settings">Settings</a>
-        <a href="/Login">Login</a>
+        <LoginButton />
+        <LogoutButton />
         <a href="/Signup">Signup</a>
   
     </section>
