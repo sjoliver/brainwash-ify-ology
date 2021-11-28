a7cea6072deb7221485084e17d7587f89482bd12
Sophie <sophiejoliver19+1@gmail.com>
2021-11-28 23:18:42 +0000

    update baseURL

--- src/axios-instance.js
+++ src/axios-instance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  // baseURL: 'https://brainwash-api.herokuapp.com/',
-  baseURL: 'http://localhost:3000/',
+  baseURL: 'https://brainwash-api.herokuapp.com/',
+  // baseURL: 'http://localhost:3000/',
   // timeout: 1000
 });
 
