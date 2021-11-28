0859ff4a27c1f342fc00bfb958218f973639f128
rengebre <russell.engebretson@gmail.com>
2021-11-28 12:46:44 -0800

    remove timeout of 1000ms for now on axios instance

--- src/axios-instance.js
+++ src/axios-instance.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   // baseURL: 'https://brainwash-api.herokuapp.com/',
   baseURL: 'http://localhost:3000/',
-  timeout: 1000
+  // timeout: 1000
 });
 
 export default instance;
