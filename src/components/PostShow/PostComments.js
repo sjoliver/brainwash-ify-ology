95ebce16829f87aafa1602984e2e7bfe4fe1ac77
Katie Herda <87781887+KatieHerda@users.noreply.github.com>
2021-11-29 14:34:40 -0700

    changed opacity on hover for avatar/username link.

--- src/components/PostShow/PostComments.js
+++ src/components/PostShow/PostComments.js
@@ -10,10 +10,11 @@ export default function PostComments (props) {
     <div className="comment">
       {commentInfo.map((obj, i) => (
         <ul key={i}>
-          <div className="avatar-username">
-            <Avatar src={obj.user.social_img} className="postShow__avatar" sx={{ width: 36, height: 36 }}/>
-            <Link to={`/profile/${obj.user.id}`} className="postShow__username"> {obj.user.username}</Link>
-          </div>
+          <Link to={`/profile/${obj.user.id}`} className="avatar-username" style={{ textDecoration: 'none' }}>
+              <Avatar src={obj.user.social_img} className="postShow__avatar" sx={{ width: 36, height: 36 }}/>
+                <span className="postShow__username">{obj.user.username}</span>
+          </Link>
+
           <div className="comment-delete">
           <span>{obj.comment.content}&nbsp;&nbsp;
           {(obj.comment.user_id === dbUser.id) && 
