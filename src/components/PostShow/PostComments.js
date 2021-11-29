c4c68590fd665c8e05fe39e2448c1428c3b87808
Katie Herda <87781887+KatieHerda@users.noreply.github.com>
2021-11-29 12:43:38 -0700

    changed commentors img to an avatar. placed avatart and handle to the left side and comment to the right. spaced delete icon away from comment.

--- src/components/PostShow/PostComments.js
+++ src/components/PostShow/PostComments.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { BsTrash } from 'react-icons/bs'
+import Avatar from '@mui/material/Avatar';
 
 export default function PostComments (props) {
   const { commentInfo, dbUser, deleteComment } = props;
 
   return (
-    <div>
+    <div className="comment">
       {commentInfo.map((obj, i) => (
         <ul key={i}>
-          <img src={obj.user.social_img} width="40"/>
-          <strong>< Link to={`/profile/${obj.user.id}`}> {obj.user.username}</Link></strong>
-          <br/>
-          {obj.comment.content}
+          <div className="avatar-username">
+            <Avatar src={obj.user.social_img} sx={{ width: 36, height: 36 }}/>
+            <Link to={`/profile/${obj.user.id}`} className="username"> {obj.user.username}</Link>
+          </div>
+          <div className="comment-delete">
+          <p>{obj.comment.content}&nbsp;&nbsp;
           {(obj.comment.user_id === dbUser.id) && 
             < BsTrash type="deleteComment" onClick={() => {deleteComment(obj.comment.id)}}/>
-          }
+          }</p>
+          </div>
         </ul>
       ))}
     </div>
