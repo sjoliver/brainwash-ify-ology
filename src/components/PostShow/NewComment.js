302abc144ce0fcf01a7b8f539f3717afe475f05f
Katie Herda <87781887+KatieHerda@users.noreply.github.com>
2021-11-29 13:03:06 -0700

    removed field set.

--- src/components/PostShow/NewComment.js
+++ src/components/PostShow/NewComment.js
@@ -4,10 +4,9 @@ export default function NewComment(props) {
   const { comment, setComment, submitComment } = props;
 
   return (
-    <div className="wrapper">
+    <div className="new-comment">
       <h1>Comment Section</h1>
       <form onSubmit={event => event.preventDefault()} >
-        <fieldset>
           <label>
             <input 
               name="comment" 
@@ -18,7 +17,6 @@ export default function NewComment(props) {
             />
           </label>
           <button type="submitComment" onClick={submitComment}>Submit</button>
-        </fieldset>
       </form>
     </div>
   )
