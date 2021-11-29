2cd0e369b65a76757c55b97e8715ee85ba3c0236
Katie Herda <87781887+KatieHerda@users.noreply.github.com>
2021-11-29 11:52:16 -0700

    refectored post show into multiple components.

---
+++ src/components/PostShow/PostContent.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { BsSuitHeartFill, BsSuitHeart } from 'react-icons/bs'
+import { Link } from 'react-router-dom';
+import { fetchImage } from '../../helpers/userHelpers';
+
+export default function PostContent(props) {
+  const { post, upload, like, likes, likePost, unlikePost, commentInfo, postUserInfo } = props;
+
+  return (
+    <>
+      <h1>{post.title}</h1>
+
+      {upload.content.includes("video") && 
+        <video width="320" height="240" controls>
+          <source src={upload.upload_file} type="video/mp4"/>
+        </video>
+      }
+
+      
+        { !like ?
+          (< BsSuitHeart type="like" onClick={likePost}/>)
+          :
+          (< BsSuitHeartFill type="unlike" onClick={unlikePost} />)
+        }
+        <strong><
+           Link to={`/profile/${postUserInfo.id}`}> {postUserInfo.username}</Link>
+        </strong>
+     
+      {post.description}
+
+      <p>Like count: {likes.length}</p>
+      <p>Comment count: {commentInfo.length}</p>
+    </>
+  )
+}
