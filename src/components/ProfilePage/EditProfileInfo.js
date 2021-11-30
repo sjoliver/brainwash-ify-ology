4f84d00be961336c0e49142a597b6bfc9351c437
Katie Herda <87781887+KatieHerda@users.noreply.github.com>
2021-11-30 16:43:05 -0700

    moved edit fields to be over top of actual info fields on edit.

--- src/components/ProfilePage/EditProfileInfo.js
+++ src/components/ProfilePage/EditProfileInfo.js
@@ -1,6 +1,8 @@
 import axios from '../../axios-instance';
 import React, { useState } from 'react'
 import InputForm from './InputForm';
+import './EditProfileInfo.scss'
+import { TextField, Button, Input } from '@mui/material';
 
 export default function EditProfileInfo(props) {
   const { localUser, setLocalUser, setMode } = props;
@@ -46,12 +48,12 @@ export default function EditProfileInfo(props) {
   }
   
   return (
-    <form onSubmit={event => event.preventDefault()}>
-      <InputForm edit={edit} setEdit={setEdit} field={"username"} placeholder={localUser.username}/>
-      <InputForm edit={edit} setEdit={setEdit} field={"bio"} placeholder={localUser.bio || "enter your bio here..."}/>
-      <InputForm edit={edit} setEdit={setEdit} field={"name"} placeholder={localUser.name}/>
-      <input type="file" name="upload_file" onChange={imgChange}/>
-      <button onClick={editUser}>Submit</button>
+    <form className="edit-form" onSubmit={event => event.preventDefault()} >
+      <TextField className="edit-form__username" variant="standard" edit={edit} setEdit={setEdit} field={"username"} placeholder={localUser.username}/>
+      <TextField variant="standard" edit={edit} setEdit={setEdit} field={"name"} placeholder={localUser.name}/>
+      <TextField variant="standard" edit={edit} setEdit={setEdit} multiline field={"bio"} placeholder={localUser.bio || "enter your bio here..."}/>
+      <Input type="file" name="upload_file" onChange={imgChange}/>
+      <Button onClick={editUser} variant="contained">Submit</Button>
     </form>
   )
 }
