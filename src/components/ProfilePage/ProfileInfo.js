725278376e2d110d137eeea836dff336391c8528
Katie Herda <87781887+KatieHerda@users.noreply.github.com>
2021-11-30 15:30:38 -0700

    removed padding from top of bio to descrease space between that and follow counts. Moved edit and follow buttons in from the right a little.

--- src/components/ProfilePage/ProfileInfo.js
+++ src/components/ProfilePage/ProfileInfo.js
@@ -23,10 +23,10 @@ export default function ProfileInfo(props) {
         }
         </div>
       </div>
-      <div className="followers-following">
-            <p><b>{follows.how_many_followers_user_has}</b> followers &nbsp;&nbsp;</p>
-            <p><b>{follows.how_many_user_is_following}</b> following</p>
-          </div>
+      <span className="followers-following">
+        <p><b>{follows.how_many_followers_user_has}</b> followers &nbsp;&nbsp;</p>
+        <p><b>{follows.how_many_user_is_following}</b> following</p>
+      </span>
       <p id="user-bio">{localUser.bio || "This is where your bio would be if you weren't such a dumb dumb"}</p>
     </div>
   )
