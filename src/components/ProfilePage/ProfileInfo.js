c0c4312604282e570ee899f05fef26243353b103
Sophie <sophiejoliver19+1@gmail.com>
2021-12-01 18:04:36 +0000

    remove default bio

--- src/components/ProfilePage/ProfileInfo.js
+++ src/components/ProfilePage/ProfileInfo.js
@@ -25,7 +25,7 @@ export default function ProfileInfo(props) {
         <p><b>{follows.how_many_followers_user_has}</b> followers &nbsp;&nbsp;</p>
         <p><b>{follows.how_many_user_is_following}</b> following</p>
       </span>
-      <p id="user-bio">{localUser.bio || "This is where your bio would be if you weren't such a dumb dumb"}</p>
+      <p id="user-bio">{localUser.bio || ""}</p>
     </div>
   )
 }
